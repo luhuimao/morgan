1ce23ad147c3a92bf39370ee27b565d18b5532fa
Brian Liu <2581015450@qq.com>
2019-08-28 10:59:46 +0800

    change

--- core/src/rpc_subscriptions.rs
+++ core/src/rpc_subscriptions.rs
@@ -349,8 +349,8 @@ pub mod tests {
         let string = transport_receiver.poll();
         println!("response : {:?}", string);
         if let Async::Ready(Some(response)) = string.unwrap() {
-            let expected = format!(r#"{{"jsonrpc":"2.0","method":"accountNotification","params":{{"result":{{"data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"difs":1,"executable":false,"owner":[2,203,81,223,225,24,34,35,203,214,138,130,144,208,35,77,63,16,87,51,47,198,115,123,98,188,19,160,0,0,0,0]}},"subscription":0}}}}"#);
-            //assert_eq!(expected, response);
+            let expected = format!(r#"{{"jsonrpc":"2.0","method":"accountNotification","params":{{"result":{{"data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"difs":1,"executable":false,"owner":[2,203,81,223,225,24,34,35,203,214,138,130,144,208,35,77,63,16,87,51,47,198,115,123,98,188,19,160,0,0,0,0],"difs1":1}},"subscription":0}}}}"#);
+            assert_eq!(expected, response);
         }
 
         subscriptions.remove_account_subscription(&sub_id);
@@ -404,8 +404,8 @@ pub mod tests {
         subscriptions.check_program(&morgan_budget_api::id(), 0, &bank_forks);
         let string = transport_receiver.poll();
         if let Async::Ready(Some(response)) = string.unwrap() {
-            let expected = format!(r#"{{"jsonrpc":"2.0","method":"programNotification","params":{{"result":["{:?}",{{"data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"difs":1,"executable":false,"owner":[2,203,81,223,225,24,34,35,203,214,138,130,144,208,35,77,63,16,87,51,47,198,115,123,98,188,19,160,0,0,0,0]}}],"subscription":0}}}}"#, alice.pubkey());
-            //assert_eq!(expected, response);
+            let expected = format!(r#"{{"jsonrpc":"2.0","method":"programNotification","params":{{"result":["{:?}",{{"data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"difs":1,"executable":false,"owner":[2,203,81,223,225,24,34,35,203,214,138,130,144,208,35,77,63,16,87,51,47,198,115,123,98,188,19,160,0,0,0,0],"difs1":1}}],"subscription":0}}}}"#, alice.pubkey());
+            assert_eq!(expected, response);
         }
 
         subscriptions.remove_program_subscription(&sub_id);
