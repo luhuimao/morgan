9d0ab998938e735ea0ced2ee97d012b0028ba8cb
Brian Liu <2581015450@qq.com>
2019-08-29 16:06:27 +0800

    channge

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,4 +1,4 @@
-use crate::rpc_subscriptions::tests::{test_check_account_subscribe, test_check_program_subscribe};
+use morgan::rpc_subscriptions::tests::{test_check_account_subscribe, test_check_program_subscribe};
 
 fn main() {
 
